b86d02fcc26264df65ab28d6f33c02ba5a703643
t2penguin <love.cat.322.21+work@gmail.com>
2023-04-02 10:31:18 +0900

    add .env

--- scripts/retrieve-block-children.cjs
+++ scripts/retrieve-block-children.cjs
@@ -2,6 +2,9 @@ const fs = require('fs');
 const { setTimeout } = require('timers/promises');
 const { Client } = require('@notionhq/client');
 
+import dotenv from "dotenv"
+dotenv.config()
+
 const notion = new Client({ auth: process.env.NOTION_API_SECRET });
 
 const requestDuration = 300;
